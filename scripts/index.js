e7d2765c82862d667f127f0e80db1df2469f98e5
조예슬 <eungang3@naver.com>
2024-01-06 23:42:46 +0900

    모바일 햄버거 메뉴 열기 기능 완성

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,17 @@
 "use strict";
 
-$(document).ready(function(){
-    $("nav").load("../components/nav.html");
-})
+$(document).ready(function () {
+  $("nav").load("../components/nav.html");
+
+  $("nav").on("click", ".hamburger-btn", function () {
+    $(".hamburger-btn").hide();
+    $(".close-btn").show();
+    $(".hamburger-menu").show()
+  });
+
+  $("nav").on("click", ".close-btn", function () {
+    $(".hamburger-btn").show();
+    $(".hamburger-menu").hide();
+    $(".close-btn").hide();
+  });
+});
