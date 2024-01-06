f5906a810b4d3f51f76caa16372d79cf56e1ce08
조예슬 <eungang3@naver.com>
2024-01-07 00:16:47 +0900

    모바일 햄버거 메뉴 및 wrapper 사이즈 조정

--- scripts/index.js
+++ scripts/index.js
@@ -6,12 +6,14 @@ $(document).ready(function () {
   $("nav").on("click", ".hamburger-btn", function () {
     $(".hamburger-btn").hide();
     $(".close-btn").show();
-    $(".hamburger-menu").show()
+    $(".hamburger-menu").show();
+    $(".navigation").css("background-color", "rgba(1, 0, 26, 1)");
   });
 
   $("nav").on("click", ".close-btn", function () {
     $(".hamburger-btn").show();
     $(".hamburger-menu").hide();
     $(".close-btn").hide();
+    $(".navigation").css("background-color", "rgba(1, 0, 26, 0.7)");
   });
 });
