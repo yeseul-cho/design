d296c2d0c9832b919f5548f2ebb55c915f811be9
조예슬 <eungang3@naver.com>
2024-01-11 00:07:55 +0900

    모달 상세 이미지 추가

--- scripts/works.js
+++ scripts/works.js
@@ -1,5 +1,27 @@
 "use strict";
 
 $(document).ready(function () {
-    const modal = ``
+  $(".image-over").click(function () {
+    const category = $(this).parent().parent().prop("className").split(" ")[1];
+    const title = $(this).siblings("img").prop("src").split("/")[6];
+    const numberOfImages = Number(
+      $(this).siblings("img").prop("className").split("-")[1]
+    );
+    let imageSrc = `./images/works/${category}/${title}`;
+
+    for (let i = 1; i <= numberOfImages; i++) {
+      $(".modal .image-area").append(`<img src="${imageSrc}/${i}.jpg">`);
+    }
+
+    $(".modal").addClass(category);
+    $(".modal").show();
+    $(".modal-background").show();
+  });
+
+  $(".close-btn").on("click", function () {
+    $(".modal").attr("class", "modal");
+    $(".modal .image-area").empty();
+    $(".modal").hide();
+    $(".modal-background").hide();
+  });
 });
